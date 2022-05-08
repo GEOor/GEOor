4fba66adcf723ef0749284697b75af8f590f76a9
kimtaehyun <kimtaehyun98@naver.com>
2022-05-09 00:48:37 +0900

    feat: Frozen Road Api

---
+++ src/main/java/geo/hs/model/Frozen/Frozen.java
@@ -0,0 +1,15 @@
+package geo.hs.model.Frozen;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Frozen {
+
+	double lat;
+	
+	double lng;
+}
