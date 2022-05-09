9c5e456253767f5c6d6dd90797fd5925c13ad560
suhwan-cheon <soo7652@naver.com>
2022-05-09 10:59:43 +0900

    feat: Include multiple markers as a single layer && made js file and apply html file

--- src/main/java/geo/hs/service/SunService.java
+++ src/main/java/geo/hs/service/SunService.java
@@ -8,13 +8,13 @@ import java.util.ArrayList;
 
 public class SunService {
 
-    private SunController sc = new SunController();
-
-    public ArrayList<ArrayList<SunInfo>> get(){
-        return sc.getSunInfo();
-    }
-
-    public void run(ArrayList<ArrayList<DsmXyz>> dem, int num, int time){
-        sc.cutSquare(dem, num, time);
-    }
+//    private SunController sc = new SunController();
+//
+//    public ArrayList<ArrayList<SunInfo>> get(){
+//        return sc.getSunInfo();
+//    }
+//
+//    public void run(ArrayList<ArrayList<DsmXyz>> dem, int num, int time){
+//        sc.cutSquare(dem, num, time);
+//    }
 }
