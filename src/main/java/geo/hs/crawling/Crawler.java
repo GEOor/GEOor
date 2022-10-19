c039494222bdab97e5483b8ed600eca3126e4500
brorica <minjea917@gmail.com>
2022-10-20 07:31:34 +0900

    front 코드 리팩토링 (#39)
    * 검색 시 Crawler에서 nullPointException이 발생하던 문제를 해결
    
    * Crawler의 오류가 발생하는 부분에 TODO 생성
    
    * Crawler.java에서 발생한 에러의 StackTree를 log 파일로 저장
    
    * HTML형 주석을 js형 주석으로 변경
    
    * addressToCoordinates 로직 개선
    
    * 사용하지 않는 wmsLayer 주석 처리
    
    * 전역변수로 선언된 map과 vectorLayer를 일반 변수에서 상수로 변경
    
    * inputDateRange 함수 로직 개선
    
    * getElement한 뒤 값을 바로 return하는 무의미한 함수 제거
    
    * addressToCoordinates에서 XML에서 값을 가져오는 공통 로직을 내부 함수로 추출
    
    * 검색 옵션에 따라 showHazardMarker를 호출하는 함수 inputTunnel, inputBridge, inputFrost를 showHazardMarker 안에 통합
    
    * setHazardMarker 함수의 로직을 단순화
    
    * createMarker에서 위/경도를 각각 인자로 받지 않고 하나의 객체로 묶어서 받음
    
    * lookAtMe의 모호한 이름과 주석을 수정
    
    * 불필요한 convertCoordinates 함수 제거
    
    * 함수 선언부에 주석을 달아 역할을 명시
    
    * body의 margin을 제거
    
    * 나중에 사용할까 싶어서 남겨뒀던 주석 덩치 제거
    
    * 아직 구현되지 않은 setHazardMarker 부분의 fetch 실행부를 주석 처리
    
    * 모든 함수를 arrow function으로 치환
    
    * request 보내는 로직과 지도를 이동하는 로직을 분리
    
    * setMapCenter에서 불필요하게 좌표계를 두 번 바꾸던 로직을 제거
    
    * 검색 결과에 따라 지도를 움직이던 함수를 addressToCoordinates 함수에 통합
    
    * 분리해낸 requestHillShade 함수에서 좌표와 행정구역 번호를 인자로 받도록 수정
    
    * checkbox input의 값을 value가 아니라 checked로 받아오도록 변경
    
    * 불필요한 log 파일 제거
    
    Co-authored-by: Themion <themion@naver.com>

--- src/main/java/geo/hs/crawling/Crawler.java
+++ src/main/java/geo/hs/crawling/Crawler.java
@@ -17,19 +17,22 @@ import java.util.concurrent.TimeUnit;
 @Getter
 @Setter
 public class Crawler {
-	
+
 	private String url = "https://astro.kasi.re.kr/life/pageView/10";
 	private String address = "crawl";
 	private Double latitude, longitude;
 	private double x_dir, y_dir;
 	private ArrayList<SunInfo> si = new ArrayList<>();
-	
-	public void run(double lat, double lng, String date){
+
+	public void run(double lat, double lng, String date) {
 		WebDriver driver = null;
 		WebElement element = null;
 
-		latitude = lat; longitude = lng; x_dir = lat; y_dir = lng;
-		
+		latitude = lat;
+		longitude = lng;
+		x_dir = lat;
+		y_dir = lng;
+
 		try {
 			// 크롬 버전에 맞게 chromedriver 설치
 			WebDriverManager.chromedriver().setup();
@@ -38,39 +41,44 @@ public class Crawler {
 			ChromeOptions chromeOptions = new ChromeOptions();
 			chromeOptions.addArguments("--headless");
 			chromeOptions.addArguments("--no-sandbox");
+
+			// TODO ChromeDriver를 설정할 때 오류가 발생하고 있습니다.
 			driver = new ChromeDriver(chromeOptions);
-			
+
 			// URL로 접속 (이때 address는 중요하지 않다. 위,경도 좌표만 제대로 입력하면 고도각이 출력된다)
-			driver.get(url+"?useElevation=1&lat="+ lat +"&lng="+ lng +"&elevation=0&output_range=1&date="+date+"&hour=&minute=&second=&address="+address);
+			driver.get(url + "?useElevation=1&lat=" + lat + "&lng=" + lng + "&elevation=0&output_range=1&date=" + date
+					+ "&hour=&minute=&second=&address=" + address);
 
 			// 대기 설정
 			driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-			
-			List<WebElement> tr = ((ChromeDriver) driver).findElementsByXPath("//*[@id=\"sun-height-table\"]/table/tbody/tr");
-			
+
+			List<WebElement> tr = ((ChromeDriver) driver)
+					.findElementsByXPath("//*[@id=\"sun-height-table\"]/table/tbody/tr");
+
 			// 출력
-//			for(int i = 0; i<tr.size(); i++) System.out.println(tr.get(i).getText());
-			
+			// for(int i = 0; i<tr.size(); i++) System.out.println(tr.get(i).getText());
+
 			crawlerParsing(tr);
 
 		} catch (Throwable e) {
 			e.printStackTrace();
 		} finally {
-			driver.quit();
+			if (driver != null)
+				driver.quit();
 		}
 	}
 
 	/**
-	 	2021-11-24
-		작성자 : 김태현
-		내용 : 태양 고도각 관련 정보 크롤링 결과 파싱
-		목적 : 크롤러 정보 get 함수로 깔끔하게 보내기 위함
-	*/
-	
-	private void crawlerParsing(List<WebElement> arr){
-		for(int i = 0; i < arr.size(); i++){
+	 * 2021-11-24
+	 * 작성자 : 김태현
+	 * 내용 : 태양 고도각 관련 정보 크롤링 결과 파싱
+	 * 목적 : 크롤러 정보 get 함수로 깔끔하게 보내기 위함
+	 */
+
+	private void crawlerParsing(List<WebElement> arr) {
+		for (int i = 0; i < arr.size(); i++) {
 			List<String> strings = Arrays.asList(arr.get(i).getText().split(" "));
-			SunInfo s = new SunInfo(0, 0, 0D, 0D, 0,0D,0D,0D,0D);
+			SunInfo s = new SunInfo(0, 0, 0D, 0D, 0, 0D, 0D, 0D, 0D);
 
 			s.setTime(i);
 			s.setLatitude(latitude);
@@ -78,7 +86,7 @@ public class Crawler {
 			s.setX(x_dir);
 			s.setY(y_dir);
 
-			for(int k = 1; k <= 12; k += 3) {
+			for (int k = 1; k <= 12; k += 3) {
 				double hour = Double.parseDouble(strings.get(k));
 				double minute = Double.parseDouble(strings.get(k + 1));
 				double second = Double.parseDouble(strings.get(k + 2));
@@ -89,14 +97,18 @@ public class Crawler {
 				// 적경을 각도로 변환. 각도 = 시간 + (분/60) + (초/3600))
 				double degree = hour + minute / 60 + second / 3600;
 
-				if (k == 1) s.setAzimuth(degree);
-				else if (k == 4) s.setAltitude(degree);
-				else if (k == 7) s.setAscension(t);
-				else s.setDeclination(degree);
+				if (k == 1)
+					s.setAzimuth(degree);
+				else if (k == 4)
+					s.setAltitude(degree);
+				else if (k == 7)
+					s.setAscension(t);
+				else
+					s.setDeclination(degree);
 			}
 
 			si.add(s);
 		}
 	}
-	
+
 }
--- src/main/resources/templates/wms.js
+++ src/main/resources/templates/wms.js
@@ -1,5 +1,6 @@
-// vworld 배경지도 띄우기
-let map = new ol.Map({
+// 대한민국 지도가 한눈에 들어오는 위치를 기준으로
+// VWorld 지도를 openlayers를 이용해 표시
+const map = new ol.Map({
     target: 'map',
     layers: [
         new ol.layer.Tile({
@@ -9,31 +10,15 @@ let map = new ol.Map({
         }),
     ],
     view: new ol.View({
-        center: convertCoordinates(127.6176, 36.8724),
+        center: ol.proj.transform([127.6176, 36.8724], 'EPSG:4326', 'EPSG:3857'),
         zoom: 7,
         minZoom: 7,
         maxZoom: 19
     })
 });
 
-// wms layer 생성
-let wmsLayer = new ol.layer.Tile({
-    visible: true,
-    source: new ol.source.TileWMS({
-        url: 'http://localhost:8600/geoserver/geor/wms',
-        params: {
-            'FORMAT': 'image/png',
-            'TILED' : true,
-            'LAYERS': 'geor:road'
-        }
-    })
-})
-
-/*
-    가장 큰 단위인 레이어 선언부
-    feature 단위의 스타일 속성을 지정할 수 있다.
- */
-let vectorLayer = new ol.layer.Vector({
+// 각 마커를 표시할 Vector 레이어
+const vectorLayer = new ol.layer.Vector({
     source: new ol.source.Vector(),
     style: new ol.style.Style({
         image: new ol.style.Icon({
@@ -43,243 +28,155 @@ let vectorLayer = new ol.layer.Vector({
     })
 });
 
-/*
-    input : 위도, 경도, id(must unique)
-    source에 넣을 feature를 생성하는 함수 (마커 생성기)
- */
-function createMarker(lng, lat, id) {
-    return new ol.Feature({
-        geometry: new ol.geom.Point(ol.proj.fromLonLat([parseFloat(lng), parseFloat(lat)])),
-        id: id
-    });
+
+// 주어진 좌표에 주어진 id를 갖는 마커 생성
+const createMarker = (coord, id) => {
+    const {latitude, longitude} = coord
+    const geometry = new ol.geom.Point(ol.proj.fromLonLat([parseFloat(longitude), parseFloat(latitude)]))
+    return new ol.Feature({ geometry, id });
 }
 
-/*
-    input: hazardName(tunnel, bridge, frozen)
-    vectorlayer - source에다가 feature(좌표, 아이콘으로 구성) 넣기
-    즉, layer - source - feature 순으로 단위가 형성되어 있다. (내림차순)
- */
-function showHazardMarker(hazardName) {
+// tunnel, bridge, frozen 중 하나를 인자로 제공한 경우
+// 각각 터널, 교량, 결빙 상태를 지도에 마커로 표시
+const setHazardMarker = async (hazardName) => {
+    const $option = document.getElementById(hazardName)
+    if (!$option.checked) return;
 
     map.addLayer(vectorLayer);
 
-    //api 호출 부분
-    fetch('http://localhost:8080/hazard/' + hazardName)
-        .then(res => res.json())
-        .then(data => {
+    try {
+        // api 호출 후 데이터를 파싱
+        const res = await fetch('http://localhost:8080/hazard/' + hazardName);
+        const data = await res.json();
 
-            let lat = [];
-            let lon = [];
+        if (data.error) return;
 
-            for(let i=0; i<data.length; i++) {
-                lat.push(data[i].latitude);
-                lon.push(data[i].longitude);
-            }
-
-            for(let i=0; i<data.length; i++) {
-                vectorLayer.getSource().addFeature(createMarker(lat[i], lon[i], i));
-            }
-        })
-        .catch(err => {
-            alert(arr);
-            //console.log(err);
-        })
-}
-
-
-function lookAtMe(lat, lng) {
-
-    /*
-        우리가 알고 있는 위,경도 좌표계(EPSG:4326) -> 구글 맵 좌표계(EPSG:3857) 변환
-        EPSG:4326..ex) (37.5, 131.2)
-     */
-    map.getView().setCenter(ol.proj.transform([lat, lng], 'EPSG:4326', 'EPSG:3857'));
-    map.getView().setZoom(16);
-}
-
-//EPSG:4326 -> EPSG:3857
-function convertCoordinates(lon, lat) {
-    let x = (lon * 20037508.34) / 180;
-    let y = Math.log(Math.tan(((90 + lat) * Math.PI) / 360)) / (Math.PI / 180);
-    y = (y * 20037508.34) / 180;
-
-    //console.log("x: " + x + " y: " + y);
-    return [x, y];
-}
-
-//주소를 xy좌표로 변환하기
-function addressToCoordinates(address) {
-    let request = new XMLHttpRequest();
-    let xCoordinate, yCoordinate, districtNumber;
-
-    request.open("GET","http://api.vworld.kr/req/address?service=address&request=getcoord&version=2.0"
-        + "&crs=epsg:3857"
-        + "&address=" + encodeURI(address) + "&refine=true&simple=false&format=xml&type=road"
-        + "&key=49EA5D21-2E61-3344-82B1-9E3F0B6C5805");
-    request.send();
-    request.onreadystatechange = function() {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-
-                // 로딩창 on
-                loadingOn()
-
-                let xml = request.responseXML;
-                //console.log(xml);
-                xCoordinate = xml.getElementsByTagName('x')[0].childNodes[0].nodeValue;
-                yCoordinate = xml.getElementsByTagName('y')[0].childNodes[0].nodeValue;
-                //행정구역 번호
-                districtNumber = xml.getElementsByTagName('level4AC')[0].childNodes[0].nodeValue;
-                districtNumber = districtNumber.substr(0, 5)
-                //console.log(districtNumber);
-
-                let lat, lng; //위, 경도
-
-                [lat, lng] = ol.proj.transform([xCoordinate, yCoordinate], 'EPSG:3857', 'EPSG:4326');
-                lookAtMe(lat, lng);
-
-                let date = inputDate();
-                let time = inputTime();
-                //api 호출 부분
-                fetch("http://localhost:8080/hillShade/", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json; charset=UTF-8"
-                    },
-                    body: JSON.stringify({
-                        latitude: lat,
-                        longitude: lng,
-                        cityId: districtNumber,
-                        date: date,
-                        time: time,
-                    }),
-                }).then((response) => {
-                    //console.log(response)
-                    // 로딩창 off
-                    loadingOff()
-                    // wms layer 생성
-                    let wmsLayer = new ol.layer.Tile({
-                        visible: true,
-                        source: new ol.source.TileWMS({
-                            url: 'http://localhost:8600/geoserver/geor/wms', //행정구역 16개 따로?
-                            params: {
-                                'FORMAT': 'image/png',
-                                'TILED' : true,
-                                'LAYERS': 'geor:road',
-                                'CQL_FILTER': 'sig_cd = ' + districtNumber
-                            }
-                        })
-                    });
-
-                    map.addLayer(wmsLayer);
-
-                });
-                return [Number(xCoordinate), Number(yCoordinate)];
-            } else {
-                alert(request.status);
-            }
-        }
+        // api 호출을 통해 얻어낸 데이터를 이용해 마커를 생성
+        data.forEach((coord, i) => vectorLayer.getSource().addFeature(createMarker(coord, i)))
+    } catch (error) {
+        console.error(error)
     }
 }
 
-//입력받는 날짜범위 수정하기
-function inputDataRange(){
-    let today = new Date();
+const requestHillShade = (latitude, longitude, districtNumber) => {
+    const $date = document.getElementById('date');
+    const $time = document.getElementById('time');
 
-    //오늘 날짜
-    let year = today.getFullYear();
-    let month = today.getMonth() + 1;
-    month = month < 10 ? '0' + month : month;
-    let date = today.getDate();
-    date = date < 10 ? '0' + date : date;
-
-    document.getElementById('date').value = String(year + '-' + month + '-' + date);
-    document.getElementById('date').min = String(year + '-' + month + '-' + date);
-
-    //일주일 뒤
-    let weekDay = today.getTime() + (7*24*60*60*1000);
-    today.setTime(weekDay);
-
-    let weekYear = today.getFullYear();
-    let weekMonth = today.getMonth() + 1;
-    weekMonth = weekMonth < 10 ? '0' + weekMonth : weekMonth;
-    let weekDate = today.getDate();
-    weekDate = weekDate < 10 ? '0' + weekDate : weekDate;
-
-    document.getElementById('date').max = String(weekYear + '-' + weekMonth + '-' + weekDate);
+    /** @todo /hillShade가 제대로 작동한다면 주석 풀고 버그 수정할 것 */
+    // await fetch("http://localhost:8080/hillShade/", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json; charset=UTF-8"
+    //     },
+    //     body: JSON.stringify({
+    //         latitude,
+    //         longitude,
+    //         cityId: districtNumber,
+    //         date: $date.value,
+    //         time: $time.value.split(":")[0],
+    //     }),
+    // });
+    //console.log(response)
 }
 
-function inputAddress(){
-    let addressInput = document.getElementById('address').value;
-    //console.log("입력한 주소: ", addressInput);
-    return addressInput;
+/** @todo request 보내는 부분과 지도 이동하는 부분 나누기 */
+// 현재로서는 역할이 명확하지 않음
+// geoserver에서 도로 데이터를 받아와 지도에 그림
+const addressToCoordinates = () => {
+    const $address = document.getElementById('address');
+
+    const request = new XMLHttpRequest();
+
+    request.open("GET","http://api.vworld.kr/req/address?service=address&request=getcoord&version=2.0"
+        + "&crs=epsg:3857"
+        + "&address=" + encodeURI($address.value) + "&refine=true&simple=false&format=xml&type=road"
+        + "&key=49EA5D21-2E61-3344-82B1-9E3F0B6C5805");
+    request.send();
+    request.onreadystatechange = /* async */ () => {
+        if (request.readyState !== 4) return
+        if (request.status < 200 || request.status >= 300)
+            return alert(request.status);
+
+        // 로딩창 on
+        loadingOn()
+
+        const { responseXML } = request
+        //console.log(responseXML);
+
+        const getXMLValue = (name) => responseXML.getElementsByTagName(name)[0].childNodes[0].nodeValue
+
+        // 검색한 지역의 좌표
+        const latitude = getXMLValue('x');
+        const longitude = getXMLValue('y');
+        // 검색한 지역의 행정구역 번호
+        const districtNumber = getXMLValue('level4AC')?.substr(0, 5);
+
+        // 검색한 지역 쪽으로 지도를 이동
+        map.getView().setCenter([parseFloat(latitude), parseFloat(longitude)]);
+        map.getView().setZoom(16);
+
+        requestHillShade(latitude, longitude, districtNumber)
+
+        // 도로 데이터를 geoserver로부터 받아와 map에 표시
+        map.addLayer(new ol.layer.Tile({
+            visible: true,
+            source: new ol.source.TileWMS({
+                url: 'http://localhost:8600/geoserver/geor/wms', //행정구역 16개 따로?
+                params: {
+                    FORMAT: 'image/png',
+                    TILED : true,
+                    LAYERS: 'geor:road',
+                    CQL_FILTER: 'sig_cd = ' + districtNumber
+                }
+            })
+        }));
+
+        // 로딩창 off
+        loadingOff()
+
+        return [Number(latitude), Number(longitude)];
+    }
 }
 
-function inputDate(){
-    let dateInput = document.getElementById('date').value;
-    //console.log("입력한 날짜: ", dateInput);
-    return dateInput;
+// 검색할 날짜의 범위를 제한
+const inputDataRange = () => {
+    const $date = document.getElementById('date')
+
+    // 오늘 날짜
+    const today = new Date();
+    // 일주일 뒤
+    const weekLater = new Date(today.getTime() + (7*24*60*60*1000));
+    
+    const dateToString = (date) => date.toISOString().split("T")[0]
+
+    $date.valueAsDate = today;
+    $date.min = dateToString(today)
+    $date.max = dateToString(weekLater);
 }
 
-function inputTime(){
-    let timeInput = document.getElementById('time').value;
-    timeInput = String(timeInput).substring(0, 2);
+// 사용자가 입력한 값을 이용해 hillShade 알고리즘 실행
+const analysisStart = () => {
 
-    //console.log("입력한 시간: ", timeInput);
-    return timeInput;
-}
-
-function inputTunnel(){
-    let tunnelOption = document.getElementById('tunnelCheck').checked;
-    //console.log("터널 옵션: ", tunnelOption);
-
-    if(tunnelOption) showHazardMarker("tunnel");
-    return tunnelOption;
-}
-
-function inputBridge(){
-    let bridgeOption = document.getElementById('bridgeCheck').checked;
-    //console.log("교량 옵션: ", bridgeOption);
-
-    if(bridgeOption) showHazardMarker("bridge");
-    return bridgeOption;
-}
-
-function inputFrost(){
-    let frostOption = document.getElementById('frostCheck').checked;
-    //console.log("결빙 옵션: ", frostOption);
-
-    if(frostOption) showHazardMarker("frozen");
-    return frostOption;
-}
-
-/*
-    분석 시작 버튼(#id:analysisButton) 클릭 이벤트 발생 시 실행되는 함수
- */
-function analysisStart(){
-
-    //이전에 생성한 마커 레이어 제거
+    // 이전에 생성한 마커 레이어 제거
     map.removeLayer(vectorLayer);
 
     //1. 사용자가 입력한 위치 -> 위,경도 변환 후 지도 내 카메라 줌
-    addressToCoordinates(inputAddress());
+    addressToCoordinates();
 
     //2. 사용자가 입력한 (위치, 날짜, 시간) -> 알맞은 wms를 받아올 수 있는 api 호출
-    inputDate();
-    inputTime();
     //map.addLayer(wmsLayer);
 
     //3. (교량, 터널, 상습결빙구역) -> 마커 생성
-    inputTunnel();
-    inputBridge();
-    inputFrost();
+    // setHazardMarker("tunnel");
+    // setHazardMarker("bridge");
+    // setHazardMarker("frozen");
 
 }
 
-<!-- 날짜 범위 설정 -->
+// 날짜 범위 설정
 inputDataRange();
 
 /* 로딩창 */
 const modal = document.getElementById("modal")
-function loadingOn() {  }
-function loadingOff() { }
+const loadingOn = () =>  { }
+const loadingOff = () =>  { }
