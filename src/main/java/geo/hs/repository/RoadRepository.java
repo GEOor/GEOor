acf1fb8bc39ebda629d931a4777d28347cb6ccf9
brorica <minjea917@gmail.com>
2022-11-11 16:41:08 +0900

    Refact hillshade (#49)
    * 검색 시 Crawler에서 nullPointException이 발생하던 문제를 해결
    
    * Crawler의 오류가 발생하는 부분에 TODO 생성
    
    * Crawler.java에서 발생한 에러의 StackTree를 log 파일로 저장
    
    * 로컬에 맞게끔 DB연결 수정
    
    * html의 일부 div를 의미에 맞는 태그인 form으로 변경
    
    * form의 onclick을 js에서 설정
    
    * html에 주석 생성
    
    * 주석 추가
    
    * API
    
    * API
    
    * Modify Mapping
    
    * create requestHillShade
    
    * /requestHillshade로 요청 보낸 주소값을 전송
    
    * a
    
    * modify request parameter
    
    * requestHillShade 메소드와 addressToCoordinates 메소드를 통합
    
    * 주석처리했던 updateHillShade 메소드를 임시로 복구
    
    * 엔드포인트 이름을 requestHillShade에서 hillShade로 변경
    
    * 사용하지 않는 print문을 제거
    
    * 버그가 발생하는 updateHillShade 메소드의 호출 부분을 주석 처리
    
    * 코드리뷰 결과 반영
    
    * PostHillShadeReq에 builder 패턴 적용
    
    * PostHillShadeReq 클래스의 builder의 각 메소드 호출 시 구분하기 편하도록 줄바꿈해줌
    
    * Fix updateHillShade
    
    * Refact: 시군구 코드 타입을 String->int로 바꿈
    
    * 주석 변경
    
    Hillshade -> Hillshade로 변경
    
    Co-authored-by: Themion <themion@naver.com>
    Co-authored-by: Dabeen Jeong <hello70825@gmail.com>

--- src/main/java/geo/hs/repository/RoadRepository.java
+++ src/main/java/geo/hs/repository/RoadRepository.java
@@ -6,6 +6,7 @@ import geo.hs.model.road.Road;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -16,24 +17,26 @@ public class RoadRepository {
 
     private final JdbcTemplate jdbcTemplate;
     private final WKB wkb = new WKB();
+    private final String sql = "SELECT origin_id FROM road_divide WHERE ST_Intersects(st_setSRID(? ::geometry, 4326), the_geom) and sig_cd = ?";
 
     public RoadRepository(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void findByGeom(HashMap<Integer, Road> roadHashMap, Hillshade hillShades) {
-        String sql = "SELECT origin_id FROM road_divide WHERE ST_Intersects(st_setSRID(? ::geometry, 4326), the_geom)";
-        jdbcTemplate.query(sql, new RowMapper<Integer>() {
-            @Override
-            public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
-                int roadId = rs.getInt(1);
-                if (!roadHashMap.containsKey(roadId)) {
-                    roadHashMap.put(roadId, new Road(roadId));
+    public void findByGeom(HashMap<Integer, Road> roadHashMap, List<Hillshade> hillShades, int cityId) {
+        for (Hillshade hillShade : hillShades) {
+            jdbcTemplate.query(sql, new RowMapper<Integer>() {
+                @Override
+                public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    int roadId = rs.getInt(1);
+                    if (!roadHashMap.containsKey(roadId)) {
+                        roadHashMap.put(roadId, new Road(roadId));
+                    }
+                    roadHashMap.get(roadId).interSects(hillShade.getHillshade().intValue());
+                    return 0;
                 }
-                roadHashMap.get(roadId).interSects(hillShades.getHillshade().intValue());
-                return 0;
-            }
-        }, wkb.convertPolygonWKB(hillShades));
+            }, wkb.convertPolygonWKB(hillShade), cityId);
+        }
     }
 
     public void updateHillShade(Road road) {
